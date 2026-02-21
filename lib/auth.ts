ccf51bb5c57f5307b8069f33969da90032688302
from rojan <from rojan>
2026-02-21 20:46:56 +0800

    fixed origin

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,14 @@ import { betterAuth } from "better-auth";
 
 export const auth = betterAuth({
   baseURL: process.env.API_URL as string,
+  trustedOrigins: [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://rendezvous-cafe.vercel.app",
+  ],
+  rateLimit: {
+    enabled: false, // 👈 disable during development/testing
+  },
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID!,
