11e49289abe1fe9ffc9290c8c0f7a2fa92744ac7
from rojan <from rojan>
2026-02-21 21:19:42 +0800

    added full call back

--- lib/auth.ts
+++ lib/auth.ts
@@ -13,8 +13,7 @@ export const auth = betterAuth({
   socialProviders: {
     google: {
       prompt: "select_account",
-      redirectURI:
-        "https://rendezvous-cafe.vercel.app",
+      redirectURI: `${process.env.BETTER_AUTH_URL}/api/auth/callback/google`,
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
